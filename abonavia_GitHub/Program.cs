89a8f86abab81880acd40f330bc0225d07525aa7
abonavia <a2208025@institutmontilivi.cat>
2023-01-19 15:33:28 +0100

    Local change

--- abonavia_GitHub/Program.cs
+++ abonavia_GitHub/Program.cs
@@ -3,5 +3,6 @@
     private static void Main(string[] args)
     {
         Console.Write("Hello, Me, it's from the internet");
+        Console.WriteLine("And this text was written from C#");
     }
 }
